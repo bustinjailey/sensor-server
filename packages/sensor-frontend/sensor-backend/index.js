0a7df3bc61b1ad3d6b4bfc98ea721b208fab4c9f
Justin Bailey <jbailey@cvent.com>
2020-10-25 09:49:33 -0700

    Moving things into separate packages

--- packages/sensor-frontend/sensor-backend/index.js
+++ packages/sensor-frontend/sensor-backend/index.js
@@ -8,11 +8,12 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { SessionMap } from './src/session/index.js';
 import { v4 as uuidv4 } from 'uuid';
+import getDataForClient from './src/data-access/index.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '../sensor-frontend/')));
 
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
@@ -21,7 +22,7 @@ wss.on('connection', function (ws) {
   ws.uuid = uuidv4();
   SessionMap.set(ws.uuid, {}); // placeholder for settings from client
   const id = setInterval(function () {
-    ws.send(JSON.stringify(process.memoryUsage()), function () {
+    ws.send(JSON.stringify(getDataForClient(ws.uuid)), function () {
       //
       // Ignore errors.
       //
@@ -40,7 +41,7 @@ wss.on('connection', function (ws) {
     let dataObj = JSON.parse(data);
     switch (dataObj.type) {
       case 'CLIENT_OPTIONS':
-        var session = SessionMap.get(ws.uuid)
+        let session = SessionMap.get(ws.uuid)
         session.clientOptions = dataObj.payload;
         console.log(SessionMap.get(ws.uuid));
     }
